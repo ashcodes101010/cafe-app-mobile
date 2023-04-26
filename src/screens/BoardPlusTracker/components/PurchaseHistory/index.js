cde48378fad4942d8a2b95852c82ef3cc66c74cb
Jennifer Guo <jenniferguo@college.harvard.edu>
2023-04-25 21:46:00 -0400

    purchase history scrolling

---
+++ src/screens/BoardPlusTracker/components/PurchaseHistory/index.js
@@ -0,0 +1,67 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useRef } from 'react'
+import { DataTable } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import {
+  styles,
+} from './styles'
+
+const PurchaseHistory = () => {
+
+  return (
+    <ScrollView vertical style={styles.scroll}>
+      <DataTable>
+          <DataTable.Header>
+              <DataTable.Title>Date</DataTable.Title>
+              <DataTable.Title style={{flex: 3}}>Location</DataTable.Title>
+              <DataTable.Title>Price</DataTable.Title>
+              <DataTable.Title style={{flex: 2}}>Method</DataTable.Title>
+          </DataTable.Header>
+          <DataTable.Row>
+              <DataTable.Cell>1/24</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>Cafe Gato Rojo</DataTable.Cell>
+              <DataTable.Cell>$5</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>BoardPlus</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+              <DataTable.Cell>2/1</DataTable.Cell>
+              <DataTable.Cell style={{flex: 3}}>SEC Cafe</DataTable.Cell>
+              <DataTable.Cell>$10</DataTable.Cell>
+              <DataTable.Cell style={{flex: 2}}>Crimson Cash</DataTable.Cell>
+          </DataTable.Row>
+      </DataTable>
+    </ScrollView>
+  )
+}
+
+export default PurchaseHistory
---
+++ src/screens/BoardPlusTracker/components/PurchaseHistory/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    scroll: {
+        height: '40%',
+        flexGrow: 0,
+    },
+  })
